93d55919805abee9ed06b0c4340a7b93daaebad1
chinmayhegde <chinmayhegde@gmail.com>
2013-02-11 12:56:15 -0500

    real

--- test_emd_real.m
+++ test_emd_real.m
@@ -11,28 +11,32 @@ addpath emd_flow
 
 % k = 15, B = 400 for a test case that breaks monotonicity?
 k = 15;
-B = 400;
+B = 150;
 shft = 5;
-thrsh = 0.5*shft;
 
 load seismic_fault_real
 
-Zfaultn = a(:,51:end); Zfaultn = Zfaultn/max(abs(Zfaultn(:)));
+Zfaultn = a(21:95,126:end); Zfaultn = Zfaultn/max(abs(Zfaultn(:)));
 
-Zfaultn = sign(Zfaultn).*(Zfaultn.^2);
+%Zfaultn = sign(Zfaultn).*abs(Zfaultn);
 
 figure(10), clf
 subplot(1,2,1), imagesc(Zfaultn), axis image %, caxis([-1 1])
 subplot(1,2,2), imagesc(Zfaultn), axis image %, caxis([-1 1])
 hold on
-scatter(faults2(:,1)-50,faults2(:,3),20,'filled')
+scatter(faults2(:,1)-125,faults2(:,3)-20,50,[0 0 0],'filled')
 
 %return
 
-
+%
 mags = double(Zfaultn.^2);
 supp = emd_flow(mags,k,B,true);
 supp = double(supp);
+
+
+thrshvec = 0:0.5:2.5;
+
+for thrsh = thrshvec
 
 % label the faults
 [i,j] = find(supp); % determine the flows
@@ -48,15 +52,25 @@ v2 = diag(j(i1,i2));
 v2sort = v2(idx);
 vx = [v1sort(1:end-1)'; v1sort(2:end)'];
 vy = [v2sort(1:end-1)'; v2sort(2:end)'];
-
 figure(2), clf, 
-subplot(1,2,1),
+subplot(1,3,1),
 imagesc(Zfaultn), axis image
 axisfortex('','Noisy input','')
+rmaxis
+subplot(1,3,2),
+imagesc(Zfaultn), axis image
+hold on
+scatter(faults2(:,1)-125,faults2(:,3)-25,50,[0 0 0],'filled')
+axisfortex('','Human labels','')
 rmaxis
 subplot(1,3,3),
 imagesc(Zfaultn), axis image
 hold on
-plot(vy,vx,'k--','LineWidth',4)
-axisfortex('','Labeled Fault(s)','')
+scatter(v2sort,v1sort,60,'k','filled')
+axisfortex('','Automatic','')
 rmaxis
+
+disp(thrsh)
+pause(2)
+
+end
